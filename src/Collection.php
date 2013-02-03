4744953e4dd211a9c24846d4ec16324ed54df9f2
Sourav Ray <me@raysourav.com>
2013-02-04 00:28:46 +0530

    Added: Bind method to collection

--- src/Collection.php
+++ src/Collection.php
@@ -9,6 +9,8 @@ namespace MongoJacket;
 
 class Collection {
 	protected $collection=null;
+	protected $callables=array();
+
 	public function __construct($db=null,$collectionname=null) {
 		if( is_null($db)){
 			throw new Exception('Conection is missing');
@@ -25,11 +27,31 @@ class Collection {
 	}
 
 	public function find($option="{}"){
-		$cursor = $this->collection->find();
+		$cursor = $this->collection->find(json_decode($option));
 		for($i=0; $i<$cursor->count();$i++){
 			echo "<code>";
 			echo json_encode(arrayToObject($cursor->getNext()));
 			echo "</code><br/>";
+		}
+	}
+
+	public function bind($functionName, $function){
+		if(!is_callable($function)){
+			throw new Exception('Bind methos should be callable');
+		} else {
+			$this->callables[$functionName]=\Closure::bind($function, $this);
+			print_r($this->callables);
+			echo "<hr/>";
+		}
+	}
+
+	public function __call($functionName, $arguments){
+		print_r($this->callables);
+		echo "<hr/>";
+		if(!isset($this->callables[$functionName])){
+			throw new Exception('Unknown method');
+		} else {
+			$this->callables[$functionName]($arguments);
 		}
 	}
 }
